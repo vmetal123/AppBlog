3410bce0bd27082da9214d139928a1161720606f
Marcial Cabrera <bladi22@yahoo.com>
2018-11-13 19:35:14 -0600

    finish second video

--- app/src/main/java/com/developmentmc/appblog/LoginActivity.java
+++ app/src/main/java/com/developmentmc/appblog/LoginActivity.java
@@ -1,13 +1,83 @@
 package com.developmentmc.appblog;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText login_txt, password_txt;
+    private Button loginBtn, registerBtn;
+    private FirebaseAuth mAuth;
+    private ProgressBar loginProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loginBtn = findViewById(R.id.login_btn);
+        registerBtn = findViewById(R.id.login_reg_btn);
+        login_txt = findViewById(R.id.login_email);
+        password_txt = findViewById(R.id.login_password);
+
+        loginProgress = findViewById(R.id.login_progress);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String loginEmail = login_txt.getText().toString();
+                String loginPassword = password_txt.getText().toString();
+
+                if(TextUtils.isEmpty(loginEmail) && TextUtils.isEmpty(loginPassword)){
+                    loginProgress.setVisibility(View.VISIBLE);
+                    mAuth.signInWithEmailAndPassword(loginEmail, loginPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                sendToMain();
+                            } else {
+                                String errorMessage = task.getException().getMessage();
+                                Toast.makeText(LoginActivity.this, "Error:" + errorMessage, Toast.LENGTH_SHORT).show();
+                            }
+                            loginProgress.setVisibility(View.GONE);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser != null){
+            sendToMain();
+        }
+    }
+
+    private void sendToMain() {
+        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
     }
 }
--- app/src/main/java/com/developmentmc/appblog/MainActivity.java
+++ app/src/main/java/com/developmentmc/appblog/MainActivity.java
@@ -4,14 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(currentUser == null){
+            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginIntent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
     }
 }
